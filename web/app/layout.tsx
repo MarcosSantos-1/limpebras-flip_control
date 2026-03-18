87ecad2a2fc641230ea833655884c48a8dbe6d4f
Marcos Silva <marcos.silva@limpasp.local>
2026-03-18 15:00:06 -0300

    :zap:Feat: Habilitado o Vercel SpeedInsights!

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "ADC Control - Sistema de Gestão ADC",
@@ -33,6 +34,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
